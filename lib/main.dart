3eb175b735a3425849200adb87b466bc7d2903a7
Iulian Oana <iulian.andreioana@gmail.com>
2020-08-09 21:03:45 +0100

    Columns and alignment

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 7.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello')
+          child: Column(
+            // This property aligns the items vertically.
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // This property aligns the items horizontally, relative to each
+            // other if they have different widths.
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                color: Colors.white,
+                child: Text('Container 1')
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: Text('Container 2')
+              ),
+              Container(
+                  height: 100.0,
+                  color: Colors.red,
+                  child: Text('Container 3')
+              ),
+            ],
           ),
         ),
       ),
