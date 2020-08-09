92333d257986ec94fbd8442fd7f60cce06a0b645
Iulian Oana <iulian.andreioana@gmail.com>
2020-08-09 22:33:15 +0100

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,28 @@ class MyApp extends StatelessWidget {
                       fontWeight: FontWeight.bold
                     ),
                   ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone,
+                          color: Colors.teal.shade900,
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                          '+44 123 456 789',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20.0,
+                            fontFamily: 'Source Sans Pro',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ],
