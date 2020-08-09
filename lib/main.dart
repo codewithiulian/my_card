e8c58b90d33ab07d2ef190d0a4a365711e12ee81
Iulian Oana <iulian.andreioana@gmail.com>
2020-08-09 22:37:16 +0100

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,28 @@ class MyApp extends StatelessWidget {
                       ],
                     ),
                   ),
+                  Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.email,
+                          color: Colors.teal.shade900,
+                        ),
+                        SizedBox(width: 10.0),
+                        Text(
+                          'iulian.andreioana@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontSize: 20.0,
+                            fontFamily: 'Source Sans Pro',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ],
