ce7b83c48bf4cbb27aed1f2f5a44dccb19e63763
Iulian Oana <iulian.andreioana@gmail.com>
2020-08-10 18:02:40 +0100

    Cards ListTiles and Divider

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 backgroundImage: AssetImage('images/profile.jpeg'),
@@ -32,28 +33,44 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'Source Sans Pro'
                 ),
               ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.phone, color: Colors.teal.shade900,),
-                    SizedBox(width: 10.0),
-                    Text('+44 1234 567 890'),
-                  ],
+              // Divider: https://api.flutter.dev/flutter/material/Divider-class.html
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
               ),
-              Container(
+              // Docs - List Tile: https://api.flutter.dev/flutter/material/ListTile-class.html
+              // Docs - Card: https://api.flutter.dev/flutter/material/Card-class.html
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.email, color: Colors.teal.shade900,),
-                    SizedBox(width: 10.0),
-                    Text('iulian.andreioana@gmail.com'),
-                  ],
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal.shade900,),
+                  title: Text(
+                    '+44 1234 567 890',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal.shade900,),
+                  title: Text(
+                    'iulian.andreioana@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'
+                    ),
+                  ),
                 ),
               ),
             ],
